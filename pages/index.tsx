9fc89b41f46935a6d0953e790f3b9c0d11f95842
Abhinay Pandey <abhinay@saaspay.app>
2022-10-14 12:12:39 +0530

    eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return (
-    <div className={'mr-auto'}>
-
-    </div>
-  )
+  return <div className={'h-100 mr-auto'}></div>
 }
 
 export default Home
